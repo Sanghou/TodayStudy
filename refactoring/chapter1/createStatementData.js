b8c6e9334bd945c02f728e93b6fa7b60f5537023
sangho Kim <tkdgh97531@gmail.com>
2021-07-04 20:22:31 +0900

    [refactoring book] module

--- refactoring/chapter1/createStatementData.js
+++ refactoring/chapter1/createStatementData.js
@@ -1,12 +1,10 @@
-function statement(invoice, plays) {
-  // 중간 데이터 구조
+module.exports = function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-  return renderPlainText(statementData, plays);
+  return statementData;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -54,66 +52,10 @@ function statement(invoice, plays) {
   }
 
   function totalVolumeCredits(data) {
-    let res = 0;
-    for (let perf of data.performances) {
-      res += perf.volumeCredits;
-    }
-    return res;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function totalAmount(data) {
-    let res = 0;
-    for (let perf of data.performances) {
-      res += perf.amount;
-    }
-    return res;
+    return data.performances.reduce((acc, p) => acc + p.amount, 0);
   }
-}
-
-// 본문 전체를 별도 함수로 추출함
-function renderPlainText(data) {
-  let result = ` 청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석) \n`;
-  }
-
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
-}
-
-const p = {
-  hamlet: { name: "Hamlet", type: "tragedy" },
-  "as-like": { name: "As You Like It", type: "comedy" },
-  othello: { name: "Othello", type: "tragedy" },
 };
-
-const invoices = {
-  customer: "BigCo",
-  performances: [
-    {
-      playID: "hamlet",
-      audience: 55,
-    },
-    {
-      playID: "as-like",
-      audience: 35,
-    },
-    {
-      playID: "othello",
-      audience: 40,
-    },
-  ],
-};
-
-console.log(statement(invoices, p));
